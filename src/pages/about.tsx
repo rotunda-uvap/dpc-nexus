fa587e59c172bbc63b48da4d7f7a792a2bde8fc4
pls4e <31322343+pls4e@users.noreply.github.com>
2023-01-13 13:58:13 -0500

    text in about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,15 +5,17 @@ import { PageProps, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = (props: PageProps) => (
+const AboutPage = (props: PageProps) => (
   <Layout>
     <SEO title="About the DPC" />
     <h1>About the Digital Publishing Collaborative</h1>
-    <section className="pb-4"> <p className="py-2">Content coming.</p></section>
+    <section className="pb-4"> <p className="py-2">Material coming soon. </p></section>
+
+    <section className="pb-4"> <p className="py-2">Currently, this site populates published documents from two projects in the Digtial Publishing Collaborative: The Julian Bond Papers Project and Howard Thurman Papers Project. </p></section>
    
     {/* <p className="p-2">Content coming ({props.path})</p> */}
  
   </Layout>
 )
 
-export default SecondPage
+export default AboutPage
